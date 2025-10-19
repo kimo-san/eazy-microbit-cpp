8584bc85f97aa74d636a813b8879baf724e7e564
kimo-san <tanya.koshova0@gmail.com>
2025-10-19 12:26:48 +0200

    Update installation.sh

--- installation.sh
+++ installation.sh
@@ -2,4 +2,5 @@
 docker pull ghcr.io/carlosperate/microbit-toolchain:latest
 cd ./.local/bin/
 wget https://raw.githubusercontent.com/kimo-san/eazy-microbit-cpp/refs/heads/main/microbit-build
+chmod +x microbit-build
 cd
